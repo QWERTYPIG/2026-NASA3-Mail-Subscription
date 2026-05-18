c45b656c2edd9d262b59353c14331db56a455955
Qwertypig <luray0601@gmail.com>
2026-05-18 23:05:50 +0800

    fix: comment syntax error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig(({ mode }) => {
       strictPort: true,
       allowedHosts: true,
       hmr: {
-        # Tells the browser to connect the WebSocket to Nginx on port 80, 
-        # which Nginx will then correctly proxy down to port 55111 internally.
+        // Tells the browser to connect the WebSocket to Nginx on port 80, 
+        // which Nginx will then correctly proxy down to port 55111 internally.
         clientPort: 80 
       }
     }
