bd6b80ec982be5c890fc9cfeb06abd2810edbc54
Qwertypig <luray0601@gmail.com>
2026-05-27 10:44:04 +0800

    fix: rename missed parts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -163,7 +163,7 @@ export default function App() {
 
             {/* 管理員專屬：別名細節頁面 */}
             <Route 
-              path="/admin/alias/:id" 
+              path="/manage/alias/:id" 
               element={
                 <ProtectedRoute user={currentUser} adminOnly={true}>
                   <AliasDetail />
